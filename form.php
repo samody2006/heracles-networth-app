7d1b5c9604b4b71430fefac9bd482f205af812c8
samody2006 <samody2006@yahoo.com>
2019-10-12 12:30:49 +0100

    contact form

---
+++ form.php
@@ -0,0 +1,25 @@
+	<?php
+						if(isset($_POST['Submit'])){
+							include 'includes/dbconnection.php';
+							$firstname = $_POST['firstname'];
+							$lastname = $_POST['lastname'];
+							$country = $_POST['country'];
+							$subject = $_POST['subject'];
+							
+							$query = "INSERT INTO users (firstname, lastname, country, subject) 
+         							 VALUES('$firstname', '$lastname', '$country', '$subject')";
+    						mysqli_query($con, $query);
+							if($query == TRUE){
+								echo "<script type = \"text/javascript\">
+											alert(\"Message Sent Successfully.\");
+											window.location = (\"contactus.html\")
+											</script>";
+							} else{
+								echo "<script type = \"text/javascript\">
+											alert(\"Registration Failed. Try Again\");
+											window.location = (\"contactus.html\")
+											</script>";
+							}
+						}
+						
+					  ?>
--- heracles_user_table.sql
+++ heracles_user_table.sql
@@ -8,3 +8,14 @@ create table if not exists `users` (
   primary key (`id`) 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+use heroku_18b50216444bfd7;
+create table if not exists `contact` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(200) NOT NULL,
+  `lastname` varchar(200) NOT NULL,
+  `country` varchar(200) NOT NULL,
+  `subject` varchar(255) NOT NULL
+  primary key (`id`) 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
